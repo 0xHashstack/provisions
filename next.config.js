45f904feb36a1713b6e4f69993df1c756124dd8f
Sahitya77 <77379621+Sahitya777@users.noreply.github.com>
2023-11-24 15:04:42 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,40 +1,11 @@
-const nextConfig = {
+/** @type {import("next").NextConfig} */
+
+module.exports = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
   swcMinify: true,
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "Permissions-Policy",
-            value:
-              "camera=(); battery=(self); geolocation=(); microphone=('https://a-domain.com')",
-          },
-          {
-            key: "Strict-Transport-Security",
-            value: "max-age=63072000; includeSubDomains; preload",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "origin-when-cross-origin",
-          },
-        ],
-      },
-    ];
-  },
-  trailingSlash: true,
-};
-
-module.exports = nextConfig;
+  output: 'export',
+  // trailingSlash: true,
+}
