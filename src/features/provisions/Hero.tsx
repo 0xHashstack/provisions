18f4d04866a7d2f13aa824d7cbc0714510e45c3d
Rupam <rupamshil111@gmail.com>
2025-01-26 22:42:18 +0530

    feat: hero updated

--- src/features/provisions/Hero.tsx
+++ src/features/provisions/Hero.tsx
@@ -1,4 +1,6 @@
 import HashTokenIconFloater from '@/assets/hashTokenIconFloater';
+import HstkLogo from '@/assets/HstkLogo';
+import VideoLogo from '@/assets/videoLogo';
 import { cn } from '@/utils/cn';
 
 const Hero = ({ addressAuthenticated }: { addressAuthenticated: boolean }) => {
@@ -44,72 +46,41 @@ const Hero = ({ addressAuthenticated }: { addressAuthenticated: boolean }) => {
 						: 'top-[13%] sm:top-[9%]'
 				)}
 			/>
-			{/* <div width="100%">
-				<Box display="flex" width="100%" justifyContent="center">
-					<Box display="flex" flexDirection="column" alignItems="center">
-						<Box borderRadius="200px" border="10px solid #3B39C9">
+			<div className="w-full">
+				<div className="flex w-full justify-center">
+					<div className="flex flex-col items-center">
+						<div className="rounded-full border-10 border-[#3B39C9]">
 							<HstkLogo />
-						</Box>
-						<Text
-							fontSize={isSmallerThan700 ? '36px' : '56px'}
-							mt="1rem"
-							fontWeight="700"
-						>
-							HSTK
-						</Text>
-					</Box>
-				</Box>
-				<Box display="flex" width="100%" justifyContent="center">
-					<Text fontSize={isSmallerThan700 ? '28px' : '44px'} mt="1rem">
-						Hashstack Provisions
-					</Text>
-				</Box>
-				<Box width="100%" display="flex" justifyContent="center">
-					<Text
-						maxW="900px"
-						mt="2rem"
-						fontSize={
-							isSmallerThan700 ? '12px' : isSmallerThan1250 ? '14px' : '16px'
-						}
-						lineHeight="20px"
-						textAlign="left"
-						color="#F0F0F0"
-						ml={isSmallerThan1000 ? '2rem' : '0'}
-					>
+						</div>
+						<p className="text-3xl sm:text-5xl mt-4 font-bold">HSTK</p>
+					</div>
+				</div>
+				<div className="flex w-full justify-center">
+					<p className="text-2xl sm:text-3xl mt-4">Hashstack Provisions</p>
+				</div>
+				<div className="w-full flex justify-center">
+					<p className="max-w-[900px] mt-8 text-sm text-left sm:text-base lg:text-lg leading-6 font-light text-[#F0F0F0] ml-8 sm:ml-0">
 						Hashstack team is excited to introduce the HSTK provisions. Over the
 						4 past years of our existence, we have been fortunate to have worked
 						with the members of various groups in the form of product users,
 						investors, community contributors who have helped advance Hashstack.
-						Through out this journey, we have incentivised your participation
+						Throughout this journey, we have incentivised your participation
 						through points, token allocation etc. Provisions page is where you
 						claim them, the HSTK tokens.
-					</Text>
-				</Box>
-				<Box
-					display="flex"
-					width="100%"
-					justifyContent="center"
-					alignItems="center"
-					mt="3rem"
-					gap="0.5rem"
-					cursor="pointer"
-				>
+					</p>
+				</div>
+				<div className="flex w-full justify-center items-center mt-12 gap-2 cursor-pointer">
 					<VideoLogo />
-					<Link
+					<a
 						href="https://app.supademo.com/demo/cm4ts6m8j0qfh6gs8dm50znl7"
 						target="_blank"
 					>
-						<Text
-							color="#00D395"
-							border="2px dotted transparent"
-							_hover={{ borderBottom: '2px dotted #00D395' }}
-							cursor="pointer"
-						>
+						<p className="text-[#00D395] border-b-2 border-transparent hover:border-[#00D395] cursor-pointer">
 							Quick guide to HSTK provisions
-						</Text>
-					</Link>
-				</Box>
-			</div> */}
+						</p>
+					</a>
+				</div>
+			</div>
 		</>
 	);
 };
