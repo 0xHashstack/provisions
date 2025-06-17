ba5e994c8fee172e302cd96c6f6ce8176e5d72b8
Rupam Shil <rupamshil111@gmail.com>
2025-06-17 13:35:33 +0530

    fix: update Next.js config for GitHub Pages subdirectory deployment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import("next").NextConfig} */
 
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+
+let assetPrefix = '';
+let basePath = '';
+
+// This logic is only executed in production builds (github actions)
+if (isGithubActions) {
+	const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+	assetPrefix = `/${repo}/`;
+	basePath = `/${repo}`;
+}
+
 module.exports = {
 	reactStrictMode: true,
 	images: {
@@ -8,4 +20,6 @@ module.exports = {
 	swcMinify: true,
 	output: 'export',
 	trailingSlash: true,
+	assetPrefix: assetPrefix,
+	basePath: basePath,
 };
